1e37eb3e220d81a92a678b8e9754603f8ef71272
Kanuri Raja Sekhar <123947661+jahraj@users.noreply.github.com>
2023-06-14 00:26:17 +0530

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -20,7 +20,7 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'mydb' );
+define( 'DB_NAME', 'jahnavi' );
 
 /** Database username */
 define( 'DB_USER', 'admin' );
@@ -29,7 +29,7 @@ define( 'DB_USER', 'admin' );
 define( 'DB_PASSWORD', 'admin123' );
 
 /** Database hostname */
-define( 'DB_HOST', 'database-1.cc3dgc9qt4sb.eu-west-2.rds.amazonaws.com' );
+define( 'DB_HOST', 'jahnavi.cpc8fmsxmqfg.us-east-2.rds.amazonaws.com' );
 
 /** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8' );
